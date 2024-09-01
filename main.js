d10cc4dc57c74d973069174a741353b08e298aac
Lily-ofthevalley <lilithvdplas@gmail.com>
2024-09-01 16:46:32 +0200

    alert
    a alert to tell you that the button doesnt work

---
+++ main.js
@@ -0,0 +1,8 @@
+//Function to only read this script when the entire html file has been loaded
+$(document).ready(function(){
+
+    $(".read-more-btn").click(function(){
+        alert("This button doesn't work because I'm to lazy to make the extra pages");
+    })
+
+    });
